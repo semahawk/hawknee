99eb04ba0bc752044affd74544266600fc8eb9e5
Szymon Urbaś <szymon.urbas@yahoo.com>
2011-03-07 19:27:33 +0100

    Modifying the generator. Honestly.. Not for use yet..

--- lib/generators/hawknee/install/install_generator.rb
+++ lib/generators/hawknee/install/install_generator.rb
@@ -1,9 +1,10 @@
 module Hawknee
 	class InstallGenerator < Rails::Generators::Base
 		source_root File.expand_path('../templates', __FILE__)
+		class_option :path, :type => :string, :default => "#{Dir.pwd}", :desc => "Sepcifies the directory where the app, you want to install Hawknee in, lives."
 		
 		def manifest
-			puts "Welllcome to Hawknee, stranger!"
+			puts "Welllcome to Hawknee installation script, stranger!\n\nInstallation to #{options[:path]}"
 		end
 	end
 end
