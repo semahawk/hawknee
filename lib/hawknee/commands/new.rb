059ed393ec3bb7f7a65919f7d7851189dae81929
Szymon Urbaś <szymon.urbas@yahoo.com>
2011-03-08 21:21:55 +0100

    Yay! Subcommand now works as well!

--- lib/hawknee/commands/new.rb
+++ lib/hawknee/commands/new.rb
@@ -1,5 +1,14 @@
 module Hawknee::Cli::Command
 	class New
+		def initialize
+			options = {}
+			opts = OptionParser.new do |opts|
+				opts.on('-n', '--name NAME', 'Sets up the name for topic|forum|category') do |name|
+					options[:name] = name
+				end
+			end.parse!
+		end
+		
 		def init
 			puts "New fired up!"
 		end
